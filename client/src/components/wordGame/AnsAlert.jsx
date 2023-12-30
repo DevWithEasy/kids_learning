84e0520745d8e1e16b85354f83521f30aa515bf6
DevWithEasy <devwitheasy@gmail.com>
2023-12-30 22:04:24 +0600

    alert anime

--- client/src/components/wordGame/AnsAlert.jsx
+++ client/src/components/wordGame/AnsAlert.jsx
@@ -1,41 +1,54 @@
 import { useEffect } from "react";
-import {motion} from 'framer-motion'
-import correct from '../../assets/correct.png'
+import { motion } from 'framer-motion'
+import correct from '../../assets/correct0.png'
 import wrong from '../../assets/wrong.png'
 import correctAudio from '../../assets/correct.mp3'
 import wrongAudio from '../../assets/wrong.mp3'
 
-const AnsAlert = ({type,ansView,setAnsView}) => {
-    useEffect(()=>{
+const AnsAlert = ({ type, ansView, setAnsView }) => {
+    useEffect(() => {
         const audio = new Audio(type === 'yes' ? correctAudio : wrongAudio)
         audio.play()
-        setTimeout(()=>{
+        setTimeout(() => {
             setAnsView(!ansView)
-        },3000)
+        }, 2000)
     })
     return (
         <motion.div
-            initial = {{
-                scale : 0,
-                opacity : 0
+            initial={{
+                scale: 0,
+                opacity: 0
             }}
-            animate = {{
-                scale : 1,
-                opacity : 1
+            animate={{
+                scale: 1,
+                opacity: 1
             }}
             transition={{
-                duration : 0.1
+                duration: 0.1
             }}
             className={`fixed top-0 left-0 h-screen w-full flex justify-center items-center ${type === 'yes' ? 'bg-blue-500/50' : 'bg-red-500/50'}`}
         >
             <div
-                className="bg-white p-4 rounded-md shadow-md"
+                // className="h-[200px] text-center bg-white p-4 rounded-md shadow-md"
             >
-                <img
+                <motion.img
+                    initial={{
+                        scale: 0,
+                        opacity: 0
+                    }}
+                    animate={{
+                        scale: 2,
+                        opacity: 1
+                    }}
+                    transition={{
+                        duration: 0.5
+                    }}
                     src={type === 'yes' ? correct : wrong}
                     className="h-[200px]"
-                />
-                
+                >
+                    
+                </motion.img>
+
             </div>
         </motion.div>
     );
