cc4170698cbfd2b54edb082365611e8683ba339e
DevWithEasy <devwitheasy@gmail.com>
2023-12-31 20:38:10 +0600

    Wellcome page add

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import WordGame from "./WordGame/WordGame";
+import Welcome from "../components/Welcome";
 
 const Home = () => {
     
     return (
         <div>
-            <WordGame/>
+            {/* <WordGame/> */}
+            <Welcome/>
         </div>
     );
 };
