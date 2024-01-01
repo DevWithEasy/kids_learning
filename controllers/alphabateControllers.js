a84381b98900ab730d6007a196f3d6e3290bcb3a
DevWithEasy <devwitheasy@gmail.com>
2024-01-01 19:02:36 +0600

    client asset delete

--- controllers/alphabateControllers.js
+++ controllers/alphabateControllers.js
@@ -1,25 +1,23 @@
-const ArAlphabet = require('../models/ArAlphabet')
-const BnAlphabet = require('../models/BnAlphabet')
-const Number = require('../models/BnNumber')
-const Day = require('../models/Day')
-const EnAlphabet = require('../models/EnAlphabet')
-const Month = require('../models/Month')
-const Season = require('../models/Season')
-const data = require('./banglaOkkor')
-
-exports.demo=async(req,res,next) =>{
+exports.get=async(req,res,next) =>{
     try {
-        data.month_english
-        .forEach(async(a,i)=>{
-            const newAlphabate = new Month({
-                ...a,
-                lang : 'en',
-                image : `/image/month_${i+1}.png`,
-                audio : `/audio/en_month_${i+1}.mp3`,
-                // video : `/video/en_num${i+1}.mp4`
-            })
-            newAlphabate.save()
+        
+        return res.status(200).json({
+            success : true,
+            status : 200,
+            message : 'Successfully completed the proccess'
         })
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            status : 500,
+            message : error.message
+        })
+    }
+}
+
+exports.update=async(req,res,next) =>{
+    try {
+        
         return res.status(200).json({
             success : true,
             status : 200,
---
+++ models/Kar.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const karSchema = mongoose.Schema({
+    letter:{
+        type: String,
+        required:true
+    },
+    kar:{
+        type: String,
+        required:true
+    },
+    image:{
+        type: String,
+        required:true
+    },
+    audio:{
+        type: String,
+        required:true
+    },
+    kar_letter:{
+        type: [
+            {
+                type: String,
+                required:true
+            }
+        ],
+        required:true,
+        default : []
+    },
+    kar_word:{
+        type: [
+            {
+                type: String,
+                required:true
+            }
+        ],
+        required:true,
+        default : []
+    },
+},{
+    timestamps:true
+})
+
+const Kar = mongoose.model('Kar',karSchema)
+module.exports = Kar
