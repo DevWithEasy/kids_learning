885d42abd4eac4eb99ec5bf72daf2f58cab72453
DevWithEasy <devwitheasy@gmail.com>
2024-01-04 00:52:30 +0600

    bangla page layout design

--- client/src/pages/bangla/Bangla.jsx
+++ client/src/pages/bangla/Bangla.jsx
@@ -1,15 +1,52 @@
 import { motion } from 'framer-motion';
 import { useState } from "react";
+import {Link} from 'react-router-dom'
 
 const Bangla = () => {
-    const [view,setView] = useState(false)
-    const [selectedId,setSelectedId] = useState(false)
-    const [letter,setLetter] = useState({})
+    
     return (
         <div
-            className={`font-kalpurush bg-gradient-to-r from-pink-500 to-pink-300 p-4 grid grid-cols-4 md:grid-cols-5 gap-4 }`}
+            className='space-y-3 font-kalpurush'
         >
-            
+            <h2
+                className='px-4 py-3 text-4xl font-semibold border-b-2'
+            >
+                বাংলা স্বরবর্ণ
+            </h2>
+            <div
+                className='p-2 space-y-2'
+            >
+                <Link
+                    to='/alphabets/bn?q=vowel'
+                    className='p-2 flex items-center space-x-3 text-xl border rounded-md'
+                >
+                    <div
+                        className='w-10 h-10 flex justify-center items-center bg-blue-100 rounded-full'
+                    >
+                        <span>
+                            অ
+                        </span>
+                    </div>
+                    <span>
+                        স্বরবর্ণ পরিচিতি
+                    </span>
+                </Link>
+                <Link
+                    to='/alphabets/bn?q=non_vowel'
+                    className='p-2 flex items-center space-x-3 text-xl border rounded-md'
+                >
+                    <div
+                        className='w-10 h-10 flex justify-center items-center bg-blue-100 rounded-full'
+                    >
+                        <span>
+                            ক
+                        </span>
+                    </div>
+                    <span>
+                        ব্যঞ্জনবর্ণ পরিচিতি
+                    </span>
+                </Link>
+            </div>
         </div>
     );
 };
