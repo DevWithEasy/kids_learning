ad61fbf711fc5dbc46327d62c5701c82af01839b
DevWithEasy <devwitheasy@gmail.com>
2024-01-05 20:00:55 +0600

    draw code refractor

---
+++ client/src/components/draw/Board.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+
+const Board = ({canvasRef,letter,strokSize,strokColor,wordVisible}) => {
+    const board = useRef()
+    const [width, setWidth] = useState(192)
+    const [height, setHeight] = useState(192)
+
+    const styles = {
+        border: "none",
+    };
+
+    useEffect(() => {
+        if (board.current) {
+            const { width, height } = board?.current.getBoundingClientRect();
+
+            setWidth(width)
+            setHeight(height)
+        }
+    }, [])
+    return (
+        <div
+                ref={board}
+                className='relative w-full h-64 flex justify-center items-center bg-white border overflow-hidden rounded'
+            >
+                <span
+                    className={`text-[200px] font-extrabold ${wordVisible ? 'opacity-5' : 'opacity-0'}`}
+                >
+                    {letter?.letter}
+                </span>
+                <div
+                    className='absolute top-0 w-full h-full overflow-hidden'
+                >
+                    <ReactSketchCanvas
+                        ref={canvasRef}
+                        width={width}
+                        height={height}
+                        strokeColor={strokColor}
+                        strokeWidth={strokSize}
+                        canvasColor='rgba(255, 255, 255, 0)'
+                        style={styles}
+                    />
+                </div>
+            </div>
+
+    );
+};
+
+export default Board;
--- client/src/components/draw/Draw.jsx
+++ client/src/components/draw/Draw.jsx
@@ -1,88 +1,52 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { ReactSketchCanvas } from 'react-sketch-canvas';
+import Board from './Board';
+
 
 const Draw = ({ letter }) => {
     const canvasRef = useRef()
-    const board = useRef()
-    const [brushRadius, setBrushRadius] = useState(12)
-    const [brushColor, setBrushColor] = useState('#000000')
-    const [clientWidth, setClientWidth] = useState(192)
-    const [clientHeight, setClientHeight] = useState(192)
+    const [strokSize, setStrokSize] = useState(12)
+    const [strokColor, setStrokColor] = useState('#000000')
     const [wordVisible, setWordVisible] = useState(true)
-
-    const styles = {
-        border: "none",
-    };
 
     function handleUpdateBrushSize(e) {
         const value = Number(e.target.value);
 
         if (value > 20)
-            setBrushRadius(20);
+            setStrokSize(20);
         else if (value < 1)
-            setBrushRadius(1);
+            setStrokSize(1);
         else
-            setBrushRadius(value);
+            setStrokSize(value);
     }
 
-    useEffect(() => {
-        if (board.current) {
-            const { width, height } = board?.current.getBoundingClientRect();
-
-            setClientWidth(width)
-            setClientHeight(height)
-        }
-    }, [])
 
     return (
         <div
             className='space-y-2'
         >
-            <div
-                ref={board}
-                className='relative w-full h-64 flex justify-center items-center bg-white border overflow-hidden rounded'
-            >
-                <span
-                    className={`text-[200px] font-extrabold ${wordVisible ? 'opacity-5' : 'opacity-0'}`}
-                >
-                    {letter?.letter}
-                </span>
-                <div
-                    className='absolute top-0 w-full h-full overflow-hidden'
-                >
-                    <ReactSketchCanvas
-                        ref={canvasRef}
-                        width={clientWidth}
-                        height={clientHeight}
-                        strokeColor={brushColor}
-                        strokeWidth={brushRadius}
-                        canvasColor='rgba(255, 255, 255, 0)'
-                        style={styles}
-                    />
-                </div>
-            </div>
+            <Board {...{canvasRef,letter,strokColor,strokSize,wordVisible}}/>
 
             <div
                 className='flex items-center space-x-2 text-sm'
             >
                 <label
                     htmlFor='color'
-                    className={`p-1 border text-[${brushColor}] rounded`}
+                    className={`p-1 border text-[${strokColor}] rounded`}
                 >
                     রঙ পরিবর্তন
                 </label>
                 <input
                     id='color'
                     type='color'
-                    value={brushColor}
-                    onChange={(e) => setBrushColor(e.target.value)}
+                    value={strokColor}
+                    onChange={(e) => setStrokColor(e.target.value)}
                     className='hidden'
                 />
                 <input
                     type='number'
                     min={0}
                     max={20}
-                    value={brushRadius}
+                    value={strokSize}
                     onChange={(e) => handleUpdateBrushSize(e)}
                     className='p-1 border rounded focus:outline-none text-center'
                 />
